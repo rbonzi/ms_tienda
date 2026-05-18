c312717e050253c89151884fa368ae08917efbcd
rbonzi <benjaminosorio902@gmail.com>
2026-05-18 00:33:45 -0400

    Añadido eureka y cambiado el port

--- src/main/java/com/tienda/tienda/TiendaApplication.java
+++ src/main/java/com/tienda/tienda/TiendaApplication.java
@@ -2,11 +2,11 @@ package com.tienda.tienda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 //@EnableFeignClients
 public class TiendaApplication {
 
